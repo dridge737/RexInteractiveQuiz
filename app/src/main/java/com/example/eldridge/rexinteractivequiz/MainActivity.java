501d45c5aeb3b8e22f6911cf99ec672f8dbbff48
dridge737 <tan_eldridge@yahoo.com>
2017-07-05 01:01:23 +0800

    Initial

---
+++ app/src/main/java/com/example/eldridge/rexinteractivequiz/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.eldridge.rexinteractivequiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, QuizWindow.class);
+        startActivity(intent);
+    }
+}
